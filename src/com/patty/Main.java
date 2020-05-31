157a01d89b8eb6b7984315319a074b12adef016b
Patrick Skelley <songkick.skelley@gmail.com>
2020-05-31 17:27:41 -0400

    first successful call to get financial statement data returned via JSON

--- src/com/patty/Main.java
+++ src/com/patty/Main.java
@@ -1,8 +1,31 @@
 package com.patty;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        try {
+            call();
+
+        } catch (IOException e){
+            e.printStackTrace();
+
+        }
+    }
+
+    static void call() throws IOException {
+        URL url = new URL("https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL?apikey=aa9b189f0b8c5b59b5802e95b9d1bcee");
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
