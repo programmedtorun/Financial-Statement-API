5460cd5a4d4453372ce4db7109a65fb080786a14
Patrick Skelley <songkick.skelley@gmail.com>
2020-06-01 15:43:15 -0400

    adds quarterly financial data retreaval, TODO iterate through POJO of quarterly data

--- src/com/patty/Main.java
+++ src/com/patty/Main.java
@@ -10,22 +10,62 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
+//        if (args.length == 0) {
+//            System.err.println ("No arguments!");
+//            System.exit(0);
+//        } else {
+//            try {
+//                System.out.println(call(args[0]));
+//            } catch (IOException e){
+//                e.printStackTrace();
+//            }
+//        }
         try {
-            System.out.println(call("AAPL"));
+//            System.out.println(yearCall("AAPL"));
+            System.out.println(quarterCall("AAPL"));
 
         } catch (IOException e){
             e.printStackTrace();
-
         }
     }
 
-    static String call(String ticker) throws IOException {
+    static String quarterCall(String ticker) throws IOException {
         ticker = ticker.toUpperCase();
         if(ticker.length() > 5 || ticker.length() < 3 || !ticker.matches("[A-Za-z]+")){
             return "Symbol entered must be letters only, and between 3 and 5 characters inclusive";
         }
+        URL url = new URL("https://financialmodelingprep.com/api/v3/income-statement/"
+                + ticker + "?period=quarter&apikey=aa9b189f0b8c5b59b5802e95b9d1bcee");
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                sb.append(line);
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        String json = sb.toString();
+        Gson gson = new Gson();
+        QuarterRetriever retriever = gson.fromJson(json, QuarterRetriever.class);
 
-        URL url = new URL("https://financialmodelingprep.com/api/v3/financials/income-statement/"
+        for(HashMap<String, String> map : retriever.getQuarters()){
+            System.out.println("quarters: " + map.get("date"));
+        }
+//        for(HashMap<String, String> map : retriever.getQuarters()){
+//            System.out.println("quarter date is: " + map.get("date"));
+//        }
+
+        return "blah";
+//      return ticker;
+    }
+
+    static String yearCall(String ticker) throws IOException {
+        ticker = ticker.toUpperCase();
+        if(ticker.length() > 5 || ticker.length() < 3 || !ticker.matches("[A-Za-z]+")){
+            return "Symbol entered must be letters only, and between 3 and 5 characters inclusive";
+        }
+        URL url = new URL("https://financialmodelingprep.com/api/v3/income-statement/"
                                + ticker + "?apikey=aa9b189f0b8c5b59b5802e95b9d1bcee");
         StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
@@ -38,7 +78,7 @@ public class Main {
         }
         String json = sb.toString();
         Gson gson = new Gson();
-        Retriever retriever = gson.fromJson(json, Retriever.class);
+        YearRetriever retriever = gson.fromJson(json, YearRetriever.class);
         System.out.println("SYMBOL IS......" + retriever.getSymbol());
         System.out.println("financials are....");
         for(HashMap<String, String> map : retriever.getFinancials()){
--- src/com/patty/YearRetriever.java
+++ src/com/patty/YearRetriever.java
@@ -7,23 +7,14 @@ import java.util.HashMap;
 /**
  * Created by patrickskelley on Jun, 2020
  */
-public class Retriever {
+public class YearRetriever {
     private String symbol;
     private ArrayList<HashMap<String, String>> financials;
 
     public String getSymbol() {
         return symbol;
     }
-
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
     public ArrayList<HashMap<String, String>> getFinancials() {
         return financials;
-    }
-
-    public void setFinancials(ArrayList<HashMap<String, String>> financials) {
-        this.financials = financials;
     }
 }
