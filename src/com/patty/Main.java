583ef3ecb2eae952fcf4edff0ffc49360eac3202
Patrick Skelley <songkick.skelley@gmail.com>
2020-06-01 16:33:29 -0400

    completes loading into QRetreaver class (needed to extend ArrayList for it to load json via gson implements qCall method

--- src/com/patty/Main.java
+++ src/com/patty/Main.java
@@ -8,16 +8,41 @@ import java.net.URL;
 import java.util.HashMap;
 
 public class Main {
-
     public static void main(String[] args) {
         try {
-            System.out.println(call("AAPL"));
-
+            System.out.println(qCall("AAPL"));
         } catch (IOException e){
             e.printStackTrace();
-
         }
     }
+
+    static String qCall(String ticker) throws IOException {
+        ticker = ticker.toUpperCase();
+        if(ticker.length() > 5 || ticker.length() < 3 || !ticker.matches("[A-Za-z]+")){
+            return "Symbol entered must be letters only, and between 3 and 5 characters inclusive";
+        }
+        URL url = new URL("https://financialmodelingprep.com/api/v3/income-statement/"
+                + ticker + "?period=quarter&apikey=aa9b189f0b8c5b59b5802e95b9d1bcee");
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
+            for (String line; (line = reader.readLine()) != null;) {
+                sb.append(line);
+                System.out.println(line);
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        String json = sb.toString();
+        System.out.println(sb.toString());
+        Gson gson = new Gson();
+        QRetriever r = gson.fromJson(json, QRetriever.class);
+        for(HashMap<String, String> map : r){
+            System.out.println("ret: " + map.get("date"));
+        }
+        return "blah";
+//      return ticker;
+    }
+
 
     static String call(String ticker) throws IOException {
         ticker = ticker.toUpperCase();
