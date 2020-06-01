01b0f2e96eb8c71fa8f9bab77c5f3b3448c83832
Patrick Skelley <songkick.skelley@gmail.com>
2020-06-01 17:55:26 -0400

    adds quarterly pull of eps and rev

--- src/com/patty/Main.java
+++ src/com/patty/Main.java
@@ -5,21 +5,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.NumberFormat;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+
+//        System.out.println("keyboard args: " + args[0]);
+
+
         try {
-            System.out.println(qCall("AAPL"));
+            qCall("AAPL");
         } catch (IOException e){
             e.printStackTrace();
         }
+
+
     }
 
-    static String qCall(String ticker) throws IOException {
+    // returns eps and revenue for most recent 8 quarters
+    static void qCall(String ticker) throws IOException {
         ticker = ticker.toUpperCase();
         if(ticker.length() > 5 || ticker.length() < 3 || !ticker.matches("[A-Za-z]+")){
-            return "Symbol entered must be letters only, and between 3 and 5 characters inclusive";
+            System.out.println("Symbol entered must be letters only, and between 3 and 5 characters inclusive");
         }
         URL url = new URL("https://financialmodelingprep.com/api/v3/income-statement/"
                 + ticker + "?period=quarter&apikey=aa9b189f0b8c5b59b5802e95b9d1bcee");
@@ -27,23 +35,28 @@ public class Main {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
             for (String line; (line = reader.readLine()) != null;) {
                 sb.append(line);
-                System.out.println(line);
             }
         } catch (IOException e){
             e.printStackTrace();
         }
+
+        NumberFormat numberFormat = NumberFormat.getInstance();
+
         String json = sb.toString();
-        System.out.println(sb.toString());
         Gson gson = new Gson();
-        QRetriever r = gson.fromJson(json, QRetriever.class);
-        for(HashMap<String, String> map : r){
-            System.out.println("ret: " + map.get("date"));
+        QRetriever retriever = gson.fromJson(json, QRetriever.class);
+        System.out.println();
+        System.out.println();
+        System.out.println("************************************");
+        for(int i = 0; i < (retriever.size() - (retriever.size() - 8)); i++){
+            System.out.println("Q-Date: " + retriever.get(i).get("date"));
+            System.out.println("\tEPS: " + retriever.get(i).get("eps"));
+            System.out.println("\tRevenue: $" + retriever.get(i).get("revenue"));
+            System.out.println("--------------------------");
         }
-        return "blah";
-//      return ticker;
     }
 
-
+    // yearly call code (currently not in use)
     static String call(String ticker) throws IOException {
         ticker = ticker.toUpperCase();
         if(ticker.length() > 5 || ticker.length() < 3 || !ticker.matches("[A-Za-z]+")){
