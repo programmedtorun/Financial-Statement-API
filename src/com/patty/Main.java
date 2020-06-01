9482b09f6afa08a7442698b15a88a3dca5b7d13c
Patrick Skelley <songkick.skelley@gmail.com>
2020-06-01 10:17:13 -0400

    adds regex matching for ticker input

--- src/com/patty/Main.java
+++ src/com/patty/Main.java
@@ -9,23 +9,42 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            call();
+            System.out.println(call("AAPL5"));
 
         } catch (IOException e){
             e.printStackTrace();
 
         }
     }
+//    static String t(String ticker){
+//        ticker = ticker.toUpperCase();
+//        if(ticker.length() > 5 || ticker.length() < 3){
+//            return "Symbol entered must be between 3 and 5 characters inclusive";
+//        }
+//
+//        if(){
+//            return "Symbol must be alphabet characters";
+//        }
+//        return ticker;
+//    }
 
-    static void call() throws IOException {
-        URL url = new URL("https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL?apikey=aa9b189f0b8c5b59b5802e95b9d1bcee");
+    static String call(String ticker) throws IOException {
+        ticker = ticker.toUpperCase();
+        if(ticker.length() > 5 || ticker.length() < 3 || !ticker.matches("[A-Za-z]+")){
+            return "Symbol entered must be letters only, and between 3 and 5 characters inclusive";
+        }
+
+        URL url = new URL("https://financialmodelingprep.com/api/v3/financials/income-statement/" + ticker + "?apikey=aa9b189f0b8c5b59b5802e95b9d1bcee");
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
             for (String line; (line = reader.readLine()) != null;) {
+                System.out.println();
                 System.out.println(line);
+                System.out.println();
             }
         } catch (IOException e){
             e.printStackTrace();
         }
+        return ticker;
     }
 }
