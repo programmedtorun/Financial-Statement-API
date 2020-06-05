44c1ec033ad046d7db9b270e562d6edc8ab17046
Patrick Skelley <songkick.skelley@gmail.com>
2020-06-05 15:56:15 -0400

    added EPS % Diff and Rev % Diff columns to output. added logic that will iterate over a shorter than 2 yrs of data list. added logic that will only calculate %diff of eps and rev if there is data for 4 qs prior.

--- src/com/patty/Main.java
+++ src/com/patty/Main.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.net.URL;
 import java.text.NumberFormat;
 import java.util.HashMap;
@@ -12,20 +14,98 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        if (args.length == 0) {
-            System.err.println ("No ticker provided! Exiting program...");
-            System.exit(0);
-        } else {
+//        if (args.length == 0) {
+//            System.err.println ("No ticker provided! Exiting program...");
+//            System.exit(0);
+//        } else {
             try {
-                qCall(args[0]);
+                QRetriever retriever = qCall("GOOG");
+                printQData(retriever, "GOOG");
             } catch (IOException e){
                 e.printStackTrace();
             }
+//        }
+//        System.out.print("some initial stuff");
+//        System.out.println("beginning part should NOT have red...."+ ANSI_RED + "text has Red background" + ANSI_RESET + "some more stuff");
+    }
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    static void printQData(QRetriever retriever, String ticker){
+        System.out.println();
+        System.out.println();
+
+        // If more than 8 quarters will change iterable to 8
+        int rSize = retriever.size();
+        if (retriever.size() >= 8){
+            rSize = retriever.size() - (retriever.size() - 8);
+        }
+        int sizeDiff = retriever.size();
+       if(sizeDiff <= 12 ){
+            sizeDiff = retriever.size() - 4;
+        }
+        System.out.println("*******************************************************************************************************");
+        System.out.println("Getting Information for " + ticker);
+        System.out.println();
+        System.out.println("\tDATE\t\t\t\tEPS\t\t\t\tEPS(%Diff)\t\t\t\tRevenue\t\t\t\tRevenue(%Diff)");
+        System.out.println("-------------------------------------------------------------------------------------------------------");
+
+        // Printing info needed
+        for(int i = 0; i < rSize; i++){
+            String epsCurr = retriever.get(i).get("eps");
+            double epsC = Double.parseDouble(epsCurr);
+            double epsCRnd = round(epsC, 2);
+            epsCurr = Double.toString(epsCRnd);
+            String revCurr = retriever.get(i).get("revenue");
+            String date = retriever.get(i).get("date");
+            String epsDiff = "no data";
+            String revDiff = "no data";
+            String ANSI_RED_EPS = "";
+            String ANSI_RED_REV = "";
+            String ANSI_RESET = "\u001B[0m";
+
+
+            if(sizeDiff >= 1){
+                String epsLast = retriever.get(i + 4).get("eps");
+                double epsL = Double.parseDouble(epsLast);
+                double epsD = ((epsC - epsL) / Math.abs(epsL)) * 100;
+                double epsDRnd = round(epsD, 2);
+                if(epsDRnd >= 25){
+                    ANSI_RED_EPS = "\u001B[31m";
+                }
+                epsDiff = Double.toString(epsDRnd);
+
+                String revLast = retriever.get(i + 4).get("revenue");
+                double revC = Double.parseDouble(revCurr);
+                double revL = Double.parseDouble(revLast);
+                double revD = ((revC - revL) / Math.abs(revL)) * 100;
+                double revDRnd = round(revD, 2);
+                if(revDRnd >= 25){
+                    ANSI_RED_REV = "\u001B[31m";
+                }
+                revDiff = Double.toString(revDRnd);
+            }
+            sizeDiff--;
+            if(!epsDiff.equals("no data")){
+                epsDiff = "%" + epsDiff;
+            }
+            if(!revDiff.equals("no data")){
+                revDiff = "%" + revDiff;
+            }
+            System.out.println("sizeDiff is: " + sizeDiff);
+            System.out.println("\t" + date + "\t\t\t" + epsCurr + "\t\t\t" + ANSI_RED_EPS + epsDiff + ANSI_RESET + "\t\t\t\t" + revCurr + "\t\t\t\t" + ANSI_RED_REV + revDiff  +ANSI_RESET);
+//            System.out.printf(format, date, epsCurr, ANSI_RED_EPS + epsDiff + ANSI_RESET, revCurr, ANSI_RED_REV + revDiff  +ANSI_RESET);
+
         }
     }
 
     // returns eps and revenue for most recent 8 quarters
-    static void qCall(String ticker) throws IOException {
+    static QRetriever qCall(String ticker) throws IOException {
         ticker = ticker.toUpperCase();
         if(ticker.length() > 5 || ticker.length() < 3 || !ticker.matches("[A-Za-z]+")){
             System.err.println("TICKER symbol entered must be letters only, and between 3 and 5 characters inclusive");
@@ -47,18 +127,7 @@ public class Main {
         String json = sb.toString();
         Gson gson = new Gson();
         QRetriever retriever = gson.fromJson(json, QRetriever.class);
-        System.out.println();
-        System.out.println();
-        System.out.println("************************************");
-        System.out.println("Getting Information for " + ticker);
-
-        // Printing info needed
-        for(int i = 0; i < (retriever.size() - (retriever.size() - 8)); i++){
-            System.out.println("Q-Date: " + retriever.get(i).get("date"));
-            System.out.println("\tEPS: " + retriever.get(i).get("eps"));
-            System.out.println("\tRevenue: $" + retriever.get(i).get("revenue"));
-            System.out.println("--------------------------");
-        }
+        return retriever;
     }
 
     // yearly call code (currently not in use)
